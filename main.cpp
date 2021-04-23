33392ddfb1870542c0c84f2f6f2c465ecb09d987
Christina LaPane <80359026+ChristinaLaPane1@users.noreply.github.com>
2021-04-23 16:19:20 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+ 
+const int MAX_SIZE =10;
+
+struct BoardType
+{
+  int boardSize[MAX_SIZE][MAX_SIZE];
+  int guess[MAX_SIZE][MAX_SIZE];
+  char ocean = '~';
+    char ship = '#';
+
+};
+
+//Pre: board contains information from struct BoardType
+//Post: randomnize a board mixed with ~ for ocean and "#" for ship
+void InitBoard(BoardType &board);
+
+//Pre: board contains a 10x10 grid
+//Post: user will guess a hit and row and col
+void Fire(BoardType &board, int &row, int &col);
+
+//Pre: board contains a 10X10 grid
+  //row and col contain information from user
+//Post: will determine if row and col contain ship or ocean
+bool isHit(BoardType &board, int row, int col);
+
+//Pre: board contains grid
+  //row and col contain ship or ocean and guess from user
+//Post: if all ship is hit then sunk is true
+bool isSunk(BoardType &board, int row, int col);
+
+int main ()
+{
+    BoardType board;
+    int row, col;
+
+
+return 0;
+}
+//**********************************************************************
+void InitBoard(BoardType &board)
+{
+  //goes through row
+    //goes through col
+      //produces either ship or ocean
+}
+//********************************************************************
+void Fire(BoardType &board, int &row, int &col)
+{
+
+  cout << "Enter coordinates of ship, separated by a space";
+  cin>>row, col;
+
+  //goes through row
+    //goes through col
+      //updates to guess array
+}
+//***************************************************************************
+bool isHit(BoardType &board, int row, int col)
+{
+//if guess[row][col] == boardSize[row][col]
+  //return true;
+  
+//else
+return false;
+}
+
+//***************************************************************************
+bool isSunk(BoardType &board, int row, int col)
+{
+// if isHit is true and all ship is destroyed, then
+  //return true;
+//else
+return false;
+}
+
+
