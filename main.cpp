c534eec28c3c785866e1bc527153d2d9aa2092f4
Christina LaPane <80359026+ChristinaLaPane1@users.noreply.github.com>
2021-05-02 09:28:24 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,14 +12,10 @@ struct BoardType
   int boardSize[MAX_SIZE][MAX_SIZE];
   int guess[MAX_SIZE][MAX_SIZE];
   char ocean = '~';
-  char ship = '#';
+  char ship = '#';  
   char shipHit = 'H';
   char miss = '.';
 };
-
-//pre: program will be up and running and prompted user for thier userchoice
-//post: program will call and use the appropriate functions. 
-void GameOptions(char userchoice);
 
 //Pre: board contains information from struct BoardType
 //Post: randomnize a board mixed with ~ for ocean and "#" for ship
@@ -29,10 +25,6 @@ void InitBoard(BoardType &board);
 //Post: user will guess a hit and row and col
 void Fire(BoardType &board, int &row, int &col);
 
-//Pre: board contains a 10X10 grid
-  //row and col contain information from user
-//Post: will determine if row and col contain ship or ocean
-bool isHit(BoardType &board, int row, int col);
 
 //Pre: board contains grid
   //row and col contain ship or ocean and guess from user
@@ -50,46 +42,82 @@ return 0;
 //**********************************************************************
 void InitBoard(BoardType &board)
 {
+ 
+ 
+ 
   //goes through row
+for(int row = 0; row < MAX_SIZE; row++)
+{
     //goes through col
-      //produces either ship or ocean
-}
+  for(int col =0; col < MAX_SIZE; col++){
+   //set board to water 
+   boardSize[row][col] = board.ocean; 
+   
+   while(boardSize)
+   {
+    //selecting random locations to place ships 
+     randRow= rand()%10; 
+     randCol = rand()%10; 
+    //check if location is exmpty or now 
+    if(boardSize[row][col] == board.ocean) 
+     boardSize[randRow][randCol] = board.ship; 
+    //I feel like this might be wrong? I wasn't sure if I needed to create a new board or still assign
+    //it to boardSize[row][col] ?? 
+   
+   
+    
+}}}}
 //********************************************************************
 void Fire(BoardType &board, int &row, int &col)
 {
+ int row= 0; 
+ int col = 0; 
 
   cout << "Enter coordinates of ship, separated by a space";
   cin>>row, col;
-
-  //goes through row
-    //goes through col
-      //updates to guess array
-}
-//***************************************************************************
-bool isHit(BoardType &board, int row, int col)
-{
-//if guess[row][col] == boardSize[row][col]
-  //change symbol found at rowcol to H to represent being hit
-  //return true;
+ 
+ switch(boardSize[row][col]{
   
-//else
-return false;
-}
+  case : board.ship //if ship then print hit and replace ocean with shipHit
+   if(boardSize[row -1][col] == board.ShipHit) //checks to see if any other space is hit 
+      {
+        cout << "Hit Again!" << endl; 
+        boardSize[row][col] = board.ShipHit; 
+   }
+  else if(boarSize[row+1][col] == board.ShipHit) 
+  {
+   cout << "Hit Again!"<<endl; 
+    boardSize[row][col] == board.ShipHit;
+  }
+  else if(boardSize[row][col+1] == board.shipHit)
+  {
+   cout << "Hit Again!" <<endl; 
+   boardSize[row][col] == board.ShipHit; 
+  }
+  else {
+   cout << "Hit!"<< endl; //no ship hit next to spot 
+   boardSize[row][col] = board.ShipHit; 
+  }
+       break; 
+  case: board.ShipHit //if ship is already hit 
+    cout << "You've already hit this ship" 
+      break; 
+  default : cout << " Miss!" << endl; //just hit ocean. 
+   
+
+  
+   
+   
+}}
+
 
 //***************************************************************************
 bool isSunk(BoardType &board, int row, int col)
 {
-// if isHit is true and all ship is destroyed, then
+// if  there is no ship left 
   //return true;
 //else
 return false;
 }
-//pre: program will be up and running and prompted user for thier userchoice
-//post: program will call and use the appropriate functions. 
-void GameOptions(char userchoice){
- //use an if else to do the appopriate action for each menu option presented to the user. This keeps the main tidy and neat. 
- // when called in main, use a do while user has not chosen to quit.
- 
- 
-}
+
 
